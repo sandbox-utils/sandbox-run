1321115eef4588a45cf08666df11af38f4b53af1
Kernc <kerncece@gmail.com>
2025-10-17 05:19:05 +0200

    BUG: Mount /etc/alternatives some /usr/bin are wired through

--- tests/smoke-test.sh
+++ tests/smoke-test.sh
@@ -3,6 +3,8 @@ set -eu
 
 . "${0%/*}/_init.sh"
 
+sandbox-run sh -c 'awk -V'  # Awk via /etc/alternatives
+
 sandbox-run sh -c 'touch "$HOME/success"'
 
 test -f .sandbox*/success  # Assert file exists
