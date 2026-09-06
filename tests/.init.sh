c33f2d910b78c6784277d56af962d98776f336e3
Kernc <kerncece@gmail.com>
2026-09-06 15:29:04 +0200

    DBG: DEBUG= PS4= support all shells with LINENO; echo $? only on error

--- tests/.init.sh
+++ tests/.init.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
+# shellcheck disable=SC2016
 PS4="$(
-    if [ "${LINENO:-}" ] && [ "${BASH_VERSION:-}" ]; then lineno=':${LINENO-}>'; fi
-    printf "\033[34;40;1m+%s${lineno:-} \033[31;1m[%s]\033[0m " "$0" '$?'
-)"
+    [ ! "${LINENO:-}" ] || lineno=':${LINENO-}'
+    printf "\033[36;40;1m+%s${lineno:-}> \033[31;1m%s\033[0m" "$0" '${?#0}') "
 export PS4
 set -eux
 
